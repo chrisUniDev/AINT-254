cee2f1ddbdc42d0753137cd93137eea7ddce0707
CHRIS-PC\chris <chrislehrer@icloud.com>
2017-10-13 23:19:28 +0100

    + Movement particle effects
    Gives you the visual cue that you are moving though space

--- Assets/Scripts/ParticleMovement.cs
+++ Assets/Scripts/ParticleMovement.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class ParticleMovement : MonoBehaviour {
 
+    private Transform m_transform;
+    private ParticleSystemRenderer m_particleSystem;
+    [SerializeField]
+    private Vector2 m_scaleRange;
+    [SerializeField]
+    private float m_posSmooth;
+    [SerializeField]
+    private float m_rotSmooth;
+    [SerializeField]
+    private PlayerController m_target;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        m_transform = transform;
+        m_particleSystem = GetComponent<ParticleSystemRenderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        m_transform.position = Vector3.Lerp(m_transform.position, m_target.transform.position, Time.deltaTime * m_posSmooth);
+
+        m_transform.rotation = Quaternion.Slerp(m_transform.rotation, m_target.transform.rotation, Time.deltaTime * m_rotSmooth);
+
+        m_particleSystem.lengthScale = Mathf.Lerp(m_scaleRange.x, m_scaleRange.y, m_target.SpeedFactor);
+    }
 }
